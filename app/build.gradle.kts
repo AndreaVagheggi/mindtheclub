6fbe0fcc224b3b0ff134977f0f1856cb49efc8fc
MindTheClub <andrea@mindtheclub.com>
2026-08-13 21:03:46 +0100

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ extensions.configure<com.android.build.api.dsl.ApplicationExtension> {
         applicationId = "com.bolimot.mindtheclub"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1024
-        versionName = "Release 1.24" +
+        versionCode = 1025
+        versionName = "Release 1.25" +
                 (if (releaseLogging) " (log)" else "") +
                 (if (noPay) " (nopay)" else "")
 
